f8d8fe299518d222e33f95f5a6581407fbf1fb15
Chris Mar <cmar@customink.com>
2020-11-20 08:09:02 -0500

    use pandas to process csv

--- pull-latest.py
+++ pull-latest.py
@@ -1,10 +1,25 @@
 import requests
+import os
+import pandas as pd
 from datetime import datetime
 
-print('requesting new csv file')
+HISTORY_CSV_FILE = 'covid-tracking-project.csv'
+
 r = requests.get("https://covidtracking.com/data/download/national-history.csv")
-with open('covid-tracking-project.csv', 'w') as f:
+with open(HISTORY_CSV_FILE, 'w') as f:
   f.write(r.text)
+
+df = pd.read_csv("covid-tracking-project.csv", index_col="date")
+df = df.sort_index(ascending=True)
+
+df['7ma'] = df["positiveIncrease"].rolling(window=7).mean()
+df['7ma'] = df["7ma"].apply(lambda x: 0 if pd.isnull(x) else int(x))
+
+print(df[['positiveIncrease', '7ma']].tail(20))
+
+df[['positiveIncrease', '7ma']].tail(20).to_csv('latest.csv')
+
+os.remove(HISTORY_CSV_FILE)
 
 html_file_name = 'current.html'
 with open(html_file_name, 'w') as html:
