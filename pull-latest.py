2a3771f067349beb346c324f6659b45a9d547214
Chris Mar <cmar@customink.com>
2020-11-20 07:55:55 -0500

    add requests module requirement

--- pull-latest.py
+++ pull-latest.py
@@ -1,4 +1,10 @@
+import requests
 from datetime import datetime
+
+print('requesting new csv file')
+r = requests.get("https://covidtracking.com/data/download/national-history.csv")
+with open('covid-tracking-project.csv', 'w') as f:
+  f.write(r.text)
 
 html_file_name = 'current.html'
 with open(html_file_name, 'w') as html:
